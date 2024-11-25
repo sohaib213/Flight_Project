b0529f6e197f90da3b906c3e0248975f998afcc6
Sohaib <sohibhassan827@gmail.com>
2024-11-25 08:53:04 +0200

    Function Search_Flight is Done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException, FileNotFoundException {
@@ -8,36 +9,50 @@ public class Main {
         Airport airport1 = new Airport(1, "Cairo", "Egypt");
         Airport airport2 = new Airport(2, "London", "England");
         Airport airport3 = new Airport(3, "Los Angles", "USA");
-
         Seat seats[] = new Seat[3];
         seats[0] = new Seat(1, "High", true);
         seats[1] = new Seat(2, "Medium", true);
         seats[2] = new Seat(3, "Low", true);
-
-
         Passenger passengers[] = new Passenger[3];
         passengers[0] = new Passenger("Sohaib", "01067700658", 1);
         passengers[1] = new Passenger("Shehab", "01151692506", 2);
         passengers[2] = new Passenger("Tony", "01225641027", 3);
-
-        Flight flights_to_Write[] = new Flight[3];
+        Flight flights_to_Write[] = new Flight[4];
         flights_to_Write[0] = new Flight(1, airport2, airport1, new Date(2024 - 1900, Calendar.NOVEMBER, 4, 6, 0),
                 new Date(2024 - 1900, Calendar.NOVEMBER, 4, 8, 0), seats, passengers);
         flights_to_Write[1] = new Flight(2, airport2, airport3, new Date(2024 - 1900, Calendar.NOVEMBER, 5, 6, 0),
                 new Date(2024 - 1900, Calendar.NOVEMBER, 5, 8, 0), seats, passengers);
         flights_to_Write[2] = new Flight(3, airport3, airport1, new Date(2024 - 1900, Calendar.NOVEMBER, 6, 6, 0),
                 new Date(2024 - 1900, Calendar.NOVEMBER, 6, 8, 0), seats, passengers);
+        flights_to_Write[3] = new Flight(4, airport2, airport1, new Date(2024 - 1900, Calendar.NOVEMBER, 5, 6, 0),
+                new Date(2024 - 1900, Calendar.NOVEMBER, 5, 8, 0), seats, passengers);
 
-        File file = new File("Flighta.txt");
+        File file = new File("Flights.txt");
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
         oos.writeObject(flights_to_Write);
         oos.flush();
         oos.close();
-
         ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
         Flight [] flights = (Flight[]) ois.readObject();
-        for(Flight a: flights)
-            System.out.println(a);
+        
+        Passenger p1 = new Passenger("Abdo", "01001277917", 5);
+
+
+        Flight []flightsFiltered = p1.flightSearch("Cairo", "London", new Date(0,
+                        Calendar.JANUARY, 0, 0, 0),
+                new Date(0, Calendar.JANUARY, 0, 0, 0) );
+
+        for(Flight a: flightsFiltered) {
+            System.out.println("D-A : " + (a.getDeapartureAirport()).getAirport_Name());
+            System.out.println("A-A : " + (a.getArrivalAirport().getAirport_Name()));
+            System.out.println("D-D : " + a.getDepartureTime());
+            System.out.println("A-D : " + a.getArrivalTime() + '\n');
+        }
+
+
+
+
+
 
 
     }
--- src/Passenger.java
+++ src/Passenger.java
@@ -1,5 +1,7 @@
-import java.io.Serializable;
+import java.io.*;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Objects;
 
 public class Passenger implements Serializable {
 
@@ -39,8 +41,53 @@ public class Passenger implements Serializable {
     public Passenger() {
     }
 
-    public Flight flightSearch(Airport dearture_Airport, Airport arrival_Airport, Date departure_Date, Date arrival_Date){
-        return new Flight();
+    public Flight []flightSearch(String dearture_Airport, String arrival_Airport, Date departure_Date, Date arrival_Date) throws IOException, ClassNotFoundException {
+
+        File file = new File("Flights.txt");
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(new FileInputStream(file));
+        } catch (IOException e) {
+            System.out.println("Cant't Find Flights.txt");
+        }
+        assert ois != null;
+        Flight [] flights = (Flight[]) ois.readObject();
+        int n = 0;
+
+        // loop to calculate number of flights filtered
+        for(int i = 0; i < flights.length; i++){
+            if(((dearture_Airport.equals(flights[i].getDeapartureAirport().getAirport_Name())) ||
+                    (dearture_Airport.equals("All"))) &&
+                    ((arrival_Airport.equals(flights[i].getArrivalAirport().getAirport_Name())) ||
+                            (arrival_Airport.equals("All"))) &&
+                    ((Objects.equals(departure_Date, flights[i].getDepartureTime())) ||
+                       (Objects.equals(departure_Date, new Date(0,Calendar.JANUARY, 0, 0, 0)))) &&
+               ((Objects.equals(arrival_Date, flights[i].getArrivalTime())) ||
+                       (Objects.equals(arrival_Date, new Date(0,Calendar.JANUARY, 0, 0, 0)))))
+            {
+                n++;
+            }
+
+        }
+        System.out.println(n);
+        Flight [] flightsToken = new Flight[n];
+        int counter = 0;
+        // loop to make new array and assigned to it the flights filtered
+        for(int i = 0; i < flights.length; i++){
+            if(((dearture_Airport.equals(flights[i].getDeapartureAirport().getAirport_Name())) ||
+                    (dearture_Airport.equals("All"))) &&
+                    ((arrival_Airport.equals(flights[i].getArrivalAirport().getAirport_Name())) ||
+                            (arrival_Airport.equals("All"))) &&
+                    ((Objects.equals(departure_Date, flights[i].getDepartureTime())) ||
+                            (Objects.equals(departure_Date, new Date(0,Calendar.JANUARY, 0, 0, 0)))) &&
+                    ((Objects.equals(arrival_Date, flights[i].getArrivalTime())) ||
+                            (Objects.equals(arrival_Date, new Date(0,Calendar.JANUARY, 0, 0, 0)))))
+            {
+                flightsToken[counter] = flights[i];
+                counter++;
+            }
+        }
+        return flightsToken;
     }
 
     public void flight_Selection(Flight flight){
